bf6fc15841442b5f02d04551ff014b78aa769a12
deekast <96024516+deekast@users.noreply.github.com>
2024-04-30 14:28:21 -0400

    lesson 3/4 deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import StarField from '../visual-effects/StarField'
 import PlanetsContainer from "./PlanetsContainer"
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const [planets, setPlanets] = useState('')
+  const [planets, setPlanets] = useState([])
+
 
   useEffect(() => {
     console.log('fetching planets...')
     fetch('http://localhost:4001/planets')
     .then(res => res.json())
     .then(data => setPlanets(data))
-  })
+  }, [])
+
+  // console.log(planets)
 
   return (
     <div className="App">
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import PlanetFacts from "./PlanetFacts"
+import React, { useState } from 'react';
+import PlanetFacts from "./PlanetFacts";
 
-function PlanetCard({planet}) {
+function PlanetCard({ planet }) {
+  const [showFacts, setShowFacts] = useState(false);
+
+  const toggleFacts = () => {
+    setShowFacts(!showFacts);
+  };
 
   return (
-    <div className="card" onClick={() => console.log('Toggle!')}>
-      <h2>{planet.name}</h2>
-      <img className="" src={planet.image} alt={planet.name} />
+    <div className="card" onClick={toggleFacts}>
+      {showFacts ? (
+        <PlanetFacts planet={planet} />
+      ) : (
+        <>
+          <h2>{planet.name}</h2>
+          <img className={planet.is_planet ? "" : "not-planet"} src={planet.image} alt={planet.name} />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default PlanetCard
+export default PlanetCard;
